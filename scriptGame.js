f1c497832234aff738f9d6f0edef63679d4d16a7
Avika Patel <avikadpatel@gmail.com>
2020-08-04 19:37:43 -0700

    Add files via upload

---
+++ scriptGame.js
@@ -0,0 +1,18 @@
+/*let quotes = [
+    "Genius is one percent inspiration and ninety-nine percent perspiration.", "You can observe a lot just by watching.","A house divided against itself cannot stand.",
+    "Difficulties increase the nearer we get to the goal.","Fate is in your hands and no one elses"];
+
+    var rand;  */
+    function setup(){
+        createCanvas(200, 200);
+        background = (0,0,0);
+        //rand = random(quotes.length);
+
+    }
+
+   function draw(){
+       //fill(255,255,255);
+      // textSize(32);
+       //text("test", 1/8*width, 1/2*height);
+       //text(""+quotes[rand], 1/8*width, 1/2*height);
+   }
